99361350bedbb31dcae9d96cd5006fa9e66789cb
lalit kumar <kumar932687@gmail.com>
2023-02-04 00:49:48 +0530

    Side bar design ready

---
+++ pages/Chemical.js
@@ -0,0 +1,10 @@
+import Layout from '@/Components/Layout'
+import React from 'react'
+
+const Chemical = () => {
+  return <Layout title="Chemical">
+    Chemical Page
+  </Layout>
+}
+
+export default Chemical
