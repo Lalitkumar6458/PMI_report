942855afd7c1c1a59022158c4f7170ed4f331f35
lalit kumar <kumar932687@gmail.com>
2023-03-19 18:42:46 +0530

    env list added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,8 +7,8 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-        clientId: '63234311325-7cgii5h8vuv0c7vdn1mmt34q0suffarj.apps.googleusercontent.com',
-        clientSecret: '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'
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET
     }),
     CredentialsProvider({
       name : "Credentials",
@@ -19,13 +19,10 @@ export const authOptions = {
           email: credentials.email
           }
           return result
-          
-
       }
   })
     // ...add more providers here
   ],
-  secret: "XH6bp/TkLvnUkQiPDEZNyHc0CV+VV5RL/n+HdVHoHN0=",
   session: {
     strategy: 'jwt',
 }
